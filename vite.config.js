603dcd92f89a7168198500933d1e81e836369c1d
Mohammed <h4ck3r.x0@gmail.com>
2024-06-03 10:24:25 +0300

    change vite settings and .env.example

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    server: {
+        host: 'https://bank-letters.test'
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.jsx',
