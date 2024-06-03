245aebfa6dd55751e7df72c5801a25ee3f4b69fa
Mohammed <h4ck3r.x0@gmail.com>
2024-06-03 10:56:29 +0300

    wip

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -7,7 +7,7 @@ export default function Dashboard({ auth }) {
             user={auth.user}
             header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">لوحة التحكم</h2>}
         >
-            <Head title="Dashboard" />
+            <Head title="لوحة التحكم" />
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
